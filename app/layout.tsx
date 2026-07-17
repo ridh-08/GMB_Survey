08a2e7a889ef36f64beff58c3c161ebfffe5fbaa
unknown <riddhi.b@ahduni.edu.in>
2026-07-17 15:36:23 +0530

    Logo Add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,33 @@
 import 'app/globals.css';
 import type { Metadata } from 'next';
 import { EB_Garamond } from 'next/font/google';
+import Image from "next/image";
+import CIILogo from "@/components/images/CII_Logo.png";
+import IMELogo from "@/components/images/IME_Logo.webp";
+
+function LogoLeft() {
+  return (
+    <div className="h-10 w-24 flex items-center justify-center">
+      <Image
+        src={CIILogo}
+        alt="CII Logo"
+        className="object-contain"
+      />
+    </div>
+  );
+}
+
+function LogoRight() {
+  return (
+    <div className="h-10 w-24 flex items-center justify-center">
+      <Image
+        src={IMELogo}
+        alt="IME Logo"
+        className="object-contain"
+      />
+    </div>
+  );
+}
 
 const garamond = EB_Garamond({
   subsets: ['latin'],
